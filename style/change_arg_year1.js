1649dcb2a5236f0d6ff332a77191bce04f89e010
Gabritorre <gabritorredev@gmail.com>
2024-04-11 14:48:35 +0200

    Add link to prog_lab project

--- style/change_arg_year1.js
+++ style/change_arg_year1.js
@@ -7,7 +7,7 @@ let courses = {
 	mat_discreta : ["Introduzione", "Logica", "Insiemi", "Relazioni", "Funzioni", "Principio di induzione", "Ricorsione e sommatorie", "Aritmetica modulare", "Numeri primi", "Calcolo combinatorio"],
 	arch_elaboratori2 : ["Nozioni introduttive", "CPU pipeline", "Memoria cache", "Memoria virtuale", "ARM ISA", "Assembly", "Input e output"],
 	calcolo2 : ["Equazioni differenziali", "Curve parametriche", "Funzioni a più variabili", "Integrali doppi"],
-	prog_lab : ["Note varie", "Compilatore", "Automatizzazioni", "Debug", "Classi", "Streams", "Templates", "Move semantics", "Iteratori"]
+	prog_lab : ["Note varie", "Compilatore", "Automatizzazioni", "Debug", "Classi", "Streams", "Templates", "Move semantics", "Iteratori", "Progetto JSON parser"]
 };
 
 function show_arguments(subj) {
@@ -20,12 +20,17 @@ function show_arguments(subj) {
 		var link_text = document.createTextNode(selected_subject[i]);
 		let elem = document.createElement("li");
 		arg_link.appendChild(link_text);
-		if(selected_subject[i] == "Progetto Snake Labyrinth") {
+		if (selected_subject[i] == "Progetto Snake Labyrinth") {
 			arg_link.href = "https://github.com/Gabritorre/labyrinth";
 			arg_link.target = "_blank";
 			arg_link.classList.add("special");
 		}
-		else{
+		else if (selected_subject[i] == "Progetto JSON parser") {
+			arg_link.href = "https://github.com/Gabritorre/json_parser";
+			arg_link.target = "_blank";
+			arg_link.classList.add("special");
+		}
+		else {
 			arg_link.href = subj + "/web_notes/" + selected_subject[i] + ".html";
 			arg_link.target = "_blank";
 		}
@@ -36,10 +41,10 @@ function show_arguments(subj) {
 }
 
 function clear_output(){
-	try{	//rimuove gli output presenti
+	try {	//rimuove gli output presenti
 		let delete_list = document.getElementById("arguments");
 		while (delete_list.firstChild) {
 			delete_list.removeChild(delete_list.lastChild);
 		}
-	}catch(e){}
+	} catch(e){}
 }
