e12a28cc514d551bfa09b3f82bd62b177ab946ea
Gabritorre <gabritorredev@gmail.com>
2024-05-08 22:27:27 +0200

    Add sis_ooperativi2 notes

--- style/change_arg_year2.js
+++ style/change_arg_year2.js
@@ -8,7 +8,7 @@ let courses = {
 	basi_dati2 : ["Anomalie e Dipendenze funzionali", "Chiavi e coperture canoniche", "Esercizi dipendenze", "Decomposizione", "Forme normali", "Esercizi normalizzazione", "Vincoli di integrità", "Trigger", "Funzioni e procedure", "Sicurezza", "Indici e viste materializzate", "Transazioni", "Applicazioni di SQL", "Applicazioni web", "NoSQL"],
 	int_uomo_macchina : ["L'uomo", "La memoria", "Il pensiero", "Il computer", "Stili di interazione", "Gestures", "Interfaccia mobile", "Creazione interfaccia mobile", "User experience", "Modelli di interazione", "Ubiquitous computing"],
 	prog_ogg2 : ["Note generali", "Classi innestate", "programmazione pseudo funzionale", "Thread", "Covarianza e controvarianza", "Programmazione ad oggetti in C++"],
-	sis_operativi2 : ["Comunicazione tra processi e thread", "Creazione di processi", "Esecuzione e terminazione", "Segnali", "Pipe", "Thread", "Sezione critica", "Thread in POSIX", "Semafori", "Problemi risolubili con semafori", "Thread in Java"]
+	sis_operativi2 : ["Comunicazione tra processi e thread", "Creazione di processi", "Esecuzione e terminazione", "Segnali", "Pipe", "Thread", "Sezione critica", "Thread in POSIX", "Semafori", "Problemi risolubili con semafori", "Thread in Java", "Deadlock"]
 };
 
 function show_arguments(subj) {
